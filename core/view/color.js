38886a119e6fde765de7fc049a3740a4321e2826
Asicath <scottpaulwilde@gmail.com>
2014-07-03 18:30:22 -0700

    color in its own file

---
+++ core/view/color.js
@@ -0,0 +1,18 @@
+var Color = function(r, g, b, a) {
+    var my = {};
+
+    my.getDrawColor = function() {
+        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + (a / 255) + ')';
+    };
+
+    my.equals = function(p) {
+        return p.getDrawColor() == my.getDrawColor();
+    };
+
+    return my;
+};
+
+Color.fromNESPalette = function(hex) {
+    var c = NESPalette["h" + hex];
+    return Color(c[0], c[1], c[2], 255);
+};
--- core/view/pixel.js
+++ core/view/pixel.js
@@ -1,24 +1,4 @@
 
-var Color = function(r, g, b, a) {
-    var my = {};
-
-    my.getDrawColor = function() {
-        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + (a / 255) + ')';
-    };
-
-    my.equals = function(p) {
-        return p.getDrawColor() == my.getDrawColor();
-    };
-
-    return my;
-};
-
-Color.fromNESPalette = function(hex) {
-    var c = NESPalette["h" + hex];
-    return Color(c[0], c[1], c[2], 255);
-};
-
-
 var Pixel = function(x, y, color) {
 
     var exports = {
