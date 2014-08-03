cca06fea61d13d0a10e423a5bdcfca04a2651e7a
asicath <scottpaulwilde@gmail.com>
2014-08-03 00:14:58 -0700

    stop downloading of unused songs

--- core/controller/sound.js
+++ core/controller/sound.js
@@ -5,9 +5,9 @@ var Music = (function() {
     my.loadAll = function() {
 
         my.eightBit = new Audio(baseUrl + "assets/music/8 Bit Legend.mp3");
-        my.normal = new Audio(baseUrl + "assets/music/ENM WAVE 1.mp3");
-        my.boss = new Audio(baseUrl + "assets/music/Boss 1 .mp3");
-        my.bossIntro = new Audio(baseUrl + "assets/music/Boss intro.mp3");
+        //my.normal = new Audio(baseUrl + "assets/music/ENM WAVE 1.mp3");
+        //my.boss = new Audio(baseUrl + "assets/music/Boss 1 .mp3");
+        //my.bossIntro = new Audio(baseUrl + "assets/music/Boss intro.mp3");
 
     };
 
