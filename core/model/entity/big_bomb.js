ff7485ae3c8e525323b05112ff43ffb69ccd19e5
asicath <scottpaulwilde@gmail.com>
2014-09-13 15:13:55 -0700

    Bomb Blast convert

--- core/model/entity/big_bomb.js
+++ core/model/entity/big_bomb.js
@@ -1,4 +1,4 @@
-define(function() {
+define(['./bomb_blast'], function(BombBlast) {
 
     return function (player) {
         var my = Entity();
--- core/model/entity/bomb_blast.js
+++ core/model/entity/bomb_blast.js
@@ -1,36 +1,40 @@
-var BombBlast = function(player) {
-    var my = Entity();
+define(function() {
 
-    my.entityType = "blast";
-    my.icon = null;
-    my.visible = false;
-    my.getFootPrint().setSize(16, 16);
+    return function (player) {
+        var my = Entity();
 
-    var executeFrame_parent = my.executeFrame;
-    my.executeFrame = function() {
-        executeFrame_parent();
+        my.entityType = "blast";
+        my.icon = null;
+        my.visible = false;
+        my.getFootPrint().setSize(16, 16);
 
-        // check for intersection with player
-        var a = my.room.getIntersectingEntities(my, 'player', null);
-        if (a) {
-            for (var i = a.length-1; i >= 0; i--) {
-                a[i].takeDamage(4, my);
+        var executeFrame_parent = my.executeFrame;
+        my.executeFrame = function () {
+            executeFrame_parent();
+
+            // check for intersection with player
+            var a = my.room.getIntersectingEntities(my, 'player', null);
+            if (a) {
+                for (var i = a.length - 1; i >= 0; i--) {
+                    a[i].takeDamage(4, my);
+                }
             }
-        }
 
-        // check for intersection
-        a = my.room.getIntersectingEntities(my, 'monster');
-        if (a) {
-            for (var i = a.length-1; i >= 0; i--) {
-                a[i].takeDamage(8, player);
+            // check for intersection
+            a = my.room.getIntersectingEntities(my, 'monster');
+            if (a) {
+                for (var i = a.length - 1; i >= 0; i--) {
+                    a[i].takeDamage(8, player);
+                }
             }
-        }
 
+        };
+
+        my.setFrameTimeout(10, function () {
+            my.room.removeEntity(my);
+        });
+
+        return my;
     };
 
-    my.setFrameTimeout(10, function() {
-        my.room.removeEntity(my);
-    });
-
-    return my;
-};
+});
