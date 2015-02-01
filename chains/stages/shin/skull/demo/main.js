4591f5c6488c7ba1829fb219eccd9870206dfb39
asicath <scottpaulwilde@gmail.com>
2015-02-01 12:09:54 -0800

    skull added, moved around demo directories

---
+++ chains/stages/shin/skull/demo/main.js
@@ -0,0 +1,51 @@
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: '../../../../../',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        view: 'core/view',
+        controller: 'core/controller'
+    }
+});
+
+requirejs([
+    'core/model/directions',
+    'core/controller/cycle',
+    'dev/demo_cycle',
+    'controller/load_sprites',
+    'controller/music',
+    'controller/sound',
+    'chains/stages/shin/skull/skull'
+], function(
+    a,
+    Cycle,
+    DemoCycle,
+    LoadSprites,
+    Music,
+    Sounds,
+    Monster
+) {
+
+    window.baseUrl = "../../../../../";
+
+    Sounds.loadAll();
+
+    //var music = new Audio(baseUrl + "chains/music/fire_in_the_arese.mp3");
+
+    LoadSprites.loadAllSprites(function() {
+
+        //music.loop = true;
+        //music.play();
+
+        var demoCycle = DemoCycle(Monster, true);
+
+        var cycle = Cycle();
+        cycle.setCurrent(demoCycle);
+        cycle.start();
+    });
+
+});
--- chains/stages/shin/skull/skull.js
+++ chains/stages/shin/skull/skull.js
@@ -24,9 +24,9 @@ define([
     LoadSprites
     ) {
 
-    var spriteInfo = LoadSprites.addSpriteSheet({url:"chains/stages/shin/chibiset/set.png", name:"miniset"});
+    var spriteInfo = LoadSprites.addSpriteSheet({url:"chains/stages/shin/skull/skull.png", name:"skull"});
 
-    return function (level) {
+    return function () {
         var my = Entity();
 
         my.icon = Icon(my, spriteInfo.spriteSheet);
@@ -37,55 +37,20 @@ define([
         PlayerHitter(my);
         Mortal(my);
         Mover(my);
-        Shooter(my);
 
-        ItemDropper(my);
-
-        my.movementSources.push(new WalkRandom(my));
         my.movementSources.push(new Push(my));
 
         my.wallSensitive = true;
+        my.life = 4;
 
-        if (level == 1) {
-            my.life = 4;
-        }
-        else if (level == 2) {
-            my.life = 8;
-            my.icon.imageOptions = ImageOptions.RedToBlue;
-        }
-
-
-        my.stepChange = 8;
 
         my.getFootPrint().setSize(16, 16);
 
-
-        my.facingSpriteBaseIndex = [0, 2, 4, 6];
-
-        var itemDropLevel = 'a';
-
-        if (Math.random() < 0.25) {
-            // Fast
-            my.speed = 1.0; // can move 40 pixels in 1s or 60 frames
-            my.changeDirectionPercent = 6 / 16;
-            my.homingPercent = 128 / 255;
-            itemDropLevel = 'b';
-        }
-        else {
-            // Slow
-            my.speed = 0.5; // can move 40 pixels in 1s or 60 frames
-            my.changeDirectionPercent = 4 / 16;
-            my.homingPercent = 64 / 255;
-        }
 
         var takeDamage_parent = my.onTakeDamage;
         my.onTakeDamage = function (entity) {
             takeDamage_parent(entity);
             my.pushFromThrust(entity.facing);
-        };
-
-        my.afterDeath = function () {
-            my.dropItem(itemDropLevel);
         };
 
         my.onPlayerHit = function(player) {
