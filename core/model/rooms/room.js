a260b808e7cfc0754924aca47693d0e5d95a2492
Asicath <scottpaulwilde@gmail.com>
2015-02-11 15:22:43 -0800

    moved back

--- core/model/rooms/room.js
+++ core/model/rooms/room.js
@@ -2,12 +2,14 @@ define([
     '../rect',
     '../position',
     '../frame_event_haver',
-    'view/color'
+    'view/color',
+    'view/image_options'
 ], function(
     Rect,
     Position,
     FrameEventHaver,
-    Color
+    Color,
+    ImageOptions
 ) {
 
     return function (data) {
@@ -23,7 +25,44 @@ define([
         my.rect = new Rect(new Position(0, 0), 256, 176, 0, 0);
         my.entities = [];
 
+        // Load from tiles if present
+        if (data.tiles) {
+            my.tiles = data.tiles;
 
+            var outsideGreen = null;
+            var outsideBrown = ImageOptions('brown').addColorSwap("1A", "17");
+            var outsideGrey = ImageOptions('grey').addColorSwap("1A", "20").addColorSwap("37", "00");
+
+            var imageOptions = [
+                outsideGreen,
+                outsideBrown,
+                outsideGrey
+            ];
+
+            // init tiles
+            for (var i = my.tiles.length - 1; i >= 0; i--) {
+                var tile = my.tiles[i];
+                tile.position = new Position(tile.x, tile.y);
+                tile.rect = new Rect(tile.position, 16, 16, 0, 0);
+
+
+                tile.sprite = SpriteSheets.outside.sprites[tile.index];
+                tile.imageOptions = imageOptions[tile.palette];
+            }
+
+            // init walls from tiles
+            for (var i = my.tiles.length - 1; i >= 0; i--) {
+                var tile = my.tiles[i];
+                if (tile.type == 'wall') {
+                    my.walls.push({rect: new Rect(new Position(tile.position.x, tile.position.y), 8, 8, 0, 0)});
+                    my.walls.push({rect: new Rect(new Position(tile.position.x + 8, tile.position.y), 8, 8, 0, 0)});
+                    my.walls.push({rect: new Rect(new Position(tile.position.x, tile.position.y + 8), 8, 8, 0, 0)});
+                    if (tile.index != 24)
+                        my.walls.push({rect: new Rect(new Position(tile.position.x + 8, tile.position.y + 8), 8, 8, 0, 0)});
+                }
+            }
+
+        }
 
         if (data.overlay) {
             my.backgroundSprite = data.overlay.sprite;
