a20a7b7f10abf5e740998cf4978bbbb1f091a5b9
Asicath <scottpaulwilde@gmail.com>
2015-02-11 15:15:38 -0800

    split off json room

--- core/model/rooms/json_room.js
+++ core/model/rooms/json_room.js
@@ -1,2 +1,58 @@
-
 // reads json and creates a room
+
+define([
+    '../rect',
+    './room',
+    'view/image_options'
+], function(
+    Rect,
+    Room,
+    ImageOptions
+) {
+
+    return function (data) {
+
+        var my = Room(data);
+
+        // Load from tiles if present
+        if (data.tiles) {
+            my.tiles = data.tiles;
+
+            var outsideGreen = null;
+            var outsideBrown = ImageOptions('brown').addColorSwap("1A", "17");
+            var outsideGrey = ImageOptions('grey').addColorSwap("1A", "20").addColorSwap("37", "00");
+
+            var imageOptions = [
+                outsideGreen,
+                outsideBrown,
+                outsideGrey
+            ];
+
+            // init tiles
+            for (var i = my.tiles.length - 1; i >= 0; i--) {
+                var tile = my.tiles[i];
+                tile.position = new Position(tile.x, tile.y);
+                tile.rect = new Rect(tile.position, 16, 16, 0, 0);
+
+
+                tile.sprite = SpriteSheets.outside.sprites[tile.index];
+                tile.imageOptions = imageOptions[tile.palette];
+            }
+
+            // init walls from tiles
+            for (var i = my.tiles.length - 1; i >= 0; i--) {
+                var tile = my.tiles[i];
+                if (tile.type == 'wall') {
+                    my.walls.push({rect: new Rect(new Position(tile.position.x, tile.position.y), 8, 8, 0, 0)});
+                    my.walls.push({rect: new Rect(new Position(tile.position.x + 8, tile.position.y), 8, 8, 0, 0)});
+                    my.walls.push({rect: new Rect(new Position(tile.position.x, tile.position.y + 8), 8, 8, 0, 0)});
+                    if (tile.index != 24)
+                        my.walls.push({rect: new Rect(new Position(tile.position.x + 8, tile.position.y + 8), 8, 8, 0, 0)});
+                }
+            }
+
+        }
+
+        return my;
+    };
+});
--- core/model/rooms/room.js
+++ core/model/rooms/room.js
@@ -1,4 +1,14 @@
-define(['../rect', '../position', '../frame_event_haver', 'view/color', 'view/image_options'], function(Rect, Position, FrameEventHaver, Color, ImageOptions) {
+define([
+    '../rect',
+    '../position',
+    '../frame_event_haver',
+    'view/color'
+], function(
+    Rect,
+    Position,
+    FrameEventHaver,
+    Color
+) {
 
     return function (data) {
 
@@ -9,48 +19,11 @@ define(['../rect', '../position', '../frame_event_haver', 'view/color', 'view/im
         var removeAfterFrame = [];
         var addAfterFrame = [];
 
-        var walls = [];
+        my.walls = [];
         my.rect = new Rect(new Position(0, 0), 256, 176, 0, 0);
         my.entities = [];
 
-        // Load from tiles if present
-        if (data.tiles) {
-            my.tiles = data.tiles;
 
-            var outsideGreen = null;
-            var outsideBrown = ImageOptions('brown').addColorSwap("1A", "17");
-            var outsideGrey = ImageOptions('grey').addColorSwap("1A", "20").addColorSwap("37", "00");
-
-            var imageOptions = [
-                outsideGreen,
-                outsideBrown,
-                outsideGrey
-            ];
-
-            // init tiles
-            for (var i = my.tiles.length - 1; i >= 0; i--) {
-                var tile = my.tiles[i];
-                tile.position = new Position(tile.x, tile.y);
-                tile.rect = new Rect(tile.position, 16, 16, 0, 0);
-
-
-                tile.sprite = SpriteSheets.outside.sprites[tile.index];
-                tile.imageOptions = imageOptions[tile.palette];
-            }
-
-            // init walls from tiles
-            for (var i = my.tiles.length - 1; i >= 0; i--) {
-                var tile = my.tiles[i];
-                if (tile.type == 'wall') {
-                    walls.push({ rect: new Rect(new Position(tile.position.x, tile.position.y), 8, 8, 0, 0) });
-                    walls.push({ rect: new Rect(new Position(tile.position.x + 8, tile.position.y), 8, 8, 0, 0) });
-                    walls.push({ rect: new Rect(new Position(tile.position.x, tile.position.y + 8), 8, 8, 0, 0) });
-                    if (tile.index != 24)
-                        walls.push({ rect: new Rect(new Position(tile.position.x + 8, tile.position.y + 8), 8, 8, 0, 0) });
-                }
-            }
-
-        }
 
         if (data.overlay) {
             my.backgroundSprite = data.overlay.sprite;
@@ -65,7 +38,7 @@ define(['../rect', '../position', '../frame_event_haver', 'view/color', 'view/im
                 for (var y = 0; y < 176; y += 8) {
                     var p = overlay.getPixel(x, y);
                     if (p.color.equals(wallColor)) {
-                        walls.push({ rect: new Rect(new Position(x, y), 8, 8, 0, 0) });
+                        my.walls.push({ rect: new Rect(new Position(x, y), 8, 8, 0, 0) });
                     }
                     else {
 
@@ -165,8 +138,8 @@ define(['../rect', '../position', '../frame_event_haver', 'view/color', 'view/im
         };
 
         my.intersectsWall = function (rect) {
-            for (var i = walls.length - 1; i >= 0; i--) {
-                if (walls[i].rect.intersects(rect)) return walls[i];
+            for (var i = my.walls.length - 1; i >= 0; i--) {
+                if (my.walls[i].rect.intersects(rect)) return my.walls[i];
             }
             return false;
         };
