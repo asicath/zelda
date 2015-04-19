dd9256f542e1c22fe9698695505826e462a3db06
asicath <scottpaulwilde@gmail.com>
2015-04-19 16:01:55 -0700

    added edge and middle of pyramid

--- chains/stages/shin/rooms/pyramid_edge.js
+++ chains/stages/shin/rooms/pyramid_edge.js
@@ -2,22 +2,23 @@ define([
     'chains/stages/shin/desert_room',
     'view/sprite_sheet',
     'core/model/entity/edge',
-    'chains/stages/shin/rooms/pyramid_edge'
+    'chains/stages/shin/rooms/pyramid_middle'
 ], function(
     ShinRoom,
     SpriteSheet,
     Edge,
-    PyramidEdge
+    PyramidMiddle
 ) {
 
-    var image = SpriteSheet({url:'chains/stages/shin/images/shin_level_backs/shin1.png', map:[{x: 0, y: 0, width: 256, height: 176}]});
+    var image = SpriteSheet({url:'chains/stages/shin/images/shin_level_backs/shin2.png', map:[{x: 0, y: 0, width: 256, height: 176}]});
     var overlay = SpriteSheet({url:'chains/stages/shin/images/desert_map.png', map:[{x: 0, y: 0, width: 256, height: 176}]});
 
-    var DesertRoom = function (Monster, randomPosition) {
+    return function (Monster, randomPosition) {
 
         var my = ShinRoom(Monster, randomPosition, image, overlay);
 
-        my.title = "desert";
+        my.title = "pyramid edge";
+
 
 
         var complete = false;
@@ -27,15 +28,14 @@ define([
 
             complete = true;
 
-            var next = PyramidEdge(Monster, randomPosition);
+            var next = PyramidMiddle(Monster, randomPosition);
 
             my.slideRoomLeft(next);
 
         }));
 
+
         return my;
     };
-
-    return DesertRoom;
 
 });
--- chains/stages/shin/rooms/pyramid_middle.js
+++ chains/stages/shin/rooms/pyramid_middle.js
@@ -1,25 +1,24 @@
 define([
     'chains/stages/shin/desert_room',
     'view/sprite_sheet',
-    'core/model/entity/edge',
-    'chains/stages/shin/rooms/pyramid_edge'
+    'core/model/entity/edge'
 ], function(
     ShinRoom,
     SpriteSheet,
-    Edge,
-    PyramidEdge
+    Edge
 ) {
 
-    var image = SpriteSheet({url:'chains/stages/shin/images/shin_level_backs/shin1.png', map:[{x: 0, y: 0, width: 256, height: 176}]});
+    var image = SpriteSheet({url:'chains/stages/shin/images/shin_level_backs/shin3.png', map:[{x: 0, y: 0, width: 256, height: 176}]});
     var overlay = SpriteSheet({url:'chains/stages/shin/images/desert_map.png', map:[{x: 0, y: 0, width: 256, height: 176}]});
 
-    var DesertRoom = function (Monster, randomPosition) {
+    return function (Monster, randomPosition) {
 
         var my = ShinRoom(Monster, randomPosition, image, overlay);
 
-        my.title = "desert";
+        my.title = "pyramid middle";
 
 
+        /*
         var complete = false;
         my.addEntity(Edge(Directions.right, function (player) {
 
@@ -27,15 +26,14 @@ define([
 
             complete = true;
 
-            var next = PyramidEdge(Monster, randomPosition);
+            var next = DesertRoom(Monster, randomPosition);
 
             my.slideRoomLeft(next);
 
         }));
+        */
 
         return my;
     };
-
-    return DesertRoom;
 
 });
